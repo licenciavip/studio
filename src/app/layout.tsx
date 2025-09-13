e443ec21882f1b8ffff20adfd02591e9cca13fb7
Hola Deyvid <infodeyvidgr@gmail.com>
2025-09-13 19:49:29 +0000

    sigue pegado arriba

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       >
         <div className="relative flex min-h-dvh flex-col">
           <Header />
-          <main className="flex-1">{children}</main>
+          <main className="flex-1 flex flex-col">{children}</main>
           <Footer />
         </div>
         <Toaster />
